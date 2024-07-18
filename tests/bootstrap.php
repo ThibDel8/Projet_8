d9b11c65f0135a7c15b89a1cfd7115db140eebf0
Thibault Delattre <delattre.thibault8@gmail.com>
2024-07-18 11:34:51 +0200

    fixed #2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,10 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$configFile = __DIR__ . '/../config/bootstrap.php';
-if (is_readable($configFile)) {
-    require $configFile;
+if (file_exists(__DIR__ . '/config/bootstrap.php')) {
+    require __DIR__ . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(__DIR__ . '/../.env');
+    (new Dotenv())->bootEnv(__DIR__ . '/.env');
 }
